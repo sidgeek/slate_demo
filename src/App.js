676c4a3de47be7df7087fb55be6ad49921119d79
sid.shi <18262272638@163.com>
2023-02-12 22:02:25 +0800

    init a simple slate example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+// 导入 React 依赖。
+import React, { useState } from 'react'
+// 导入 Slate 编辑器工厂。
+import { createEditor } from 'slate'
+// 导入 Slate 组件和 React 插件。
+import { Slate, Editable, withReact } from 'slate-react'
+
+const initialValue = [{
+  type: 'paragraph',
+  children: [{ text: 'A line of text in a paragraph.' }],
+},]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // 创建一个不会在渲染中变化的 Slate 编辑器对象。
+  const [editor] = useState(() => withReact(createEditor()))
+  return <Slate editor={editor} value={initialValue}>
+    <Editable />
+  </Slate>
 }
 
 export default App;
